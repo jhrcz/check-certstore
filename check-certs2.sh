ea255946534a538e12fa39c9be8aced80b1b27e1
tailor <jahor@deebee>
2008-11-13 21:21:36 +0100

    enhanced script output, colorization

--- check-certs2.sh
+++ check-certs2.sh
@@ -5,6 +5,17 @@
 supportedCertTypes="pem|crt|p12|jks"
 exitStatus=0
 
+BLUE='\E[34m'
+RED='\E[31m'
+YELLOW='\E[33m'
+GREEN='\E[32m'
+WHITE='\E[37m'
+
+# reset terminal to standard colors
+function resetColor
+{
+	tput sgr0
+}
 function ERROR
 {
 	echo "ERROR: $1" 1>&2
@@ -59,7 +70,8 @@ echo "Checking validity of certificates:"
 
 for certFile in "${certFiles[@]}"
 do
-	echo "[cert $certFile]"
+	#echo "[cert $certFile]"
+	echo -e "${BLUE}[cert $certFile]" ; resetColor
 	certFileTXT="${certFile}.txt"
 	if [ -f "${certFileTXT}" -a "${certFileTXT}" -nt "${certFile}" ]
 	then
@@ -72,10 +84,33 @@ do
 				[ -n "$not_after" ] || ERROR "could not detect not-after date from cert in txt from"
 				if [ -n "$not_before" -a -n "$not_after" ]
 				then
+					echo " type: $certType"
+					issuer=$(grep "Issuer" $certFileTXT | cut -d : -f 2- | sed -e "s#^ ##" | head -n 1 )
+					subject=$(grep "Subject" $certFileTXT | cut -d : -f 2-  | sed -e "s#^ ##" | head -n 1 )
+					echo " subject: $subject"
+					echo " issuer: $issuer"
+					# convert date format YYYY-MM-DD
 					not_before=$(date -d "$not_before" +%Y-%m-%d)
 					not_after=$(date -d "$not_after" +%Y-%m-%d)
 					echo " not-before: $not_before"
-					echo " not-after:  $not_after"
+					echo " not-after: $not_after"
+					# check expiration date
+					if [ "$(date -d "$not_after" +%s)" -lt "$(date -d "today" +%s)" ]
+					then
+						exitStatus=1
+						#echo " status: CRITICAL"
+						echo -e " ${RED}status: CRITICAL" ; resetColor
+					else
+						if [ "$(date -d "$not_after" +%s)" -lt "$(date -d "today + 1 month" +%s)" ]
+						then
+							exitStatus=1
+							#echo " status: WARNING"
+							echo -e " ${YELLOW}status: WARNING" ; resetColor
+						else
+							#echo " status: ok"
+							echo -e " ${GREEN}status: ok" ; resetColor
+						fi	
+					fi
 				fi
 				;;
 		esac
